676e685d2b63ef479e9f8abbdad54405bd07ae96
aryanmaheshwariantino <160240685+aryanmaheshwariantino@users.noreply.github.com>
2024-07-30 12:17:18 +0530

    add ui screen and implementation

--- app/src/main/java/com/student/competishun/ui/main/HomeActivity.kt
+++ app/src/main/java/com/student/competishun/ui/main/HomeActivity.kt
@@ -5,17 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.student.competishun.R
 import com.student.competishun.databinding.ActivityHomeBinding
+import com.student.competishun.ui.fragment.CallingSupport
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var binding: ActivityHomeBinding
+    private var isCallingSupportFragmentVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,10 +25,12 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.igContactImage.setOnClickListener {
-            binding.igContactImage.setImageResource(R.drawable.fab_icon)
-            findNavController(R.id.fragmentNavigation).navigate(R.id.callingFragment)
+            if (isCallingSupportFragmentVisible) {
+                closeCallingSupportFragment()
+            } else {
+                openCallingSupportFragment()
+            }
         }
-
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentNavigation) as NavHostFragment
         navController = navHostFragment.navController
@@ -36,5 +40,17 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun openCallingSupportFragment() {
+        val callingSupportFragment = CallingSupport()
+        callingSupportFragment.show(supportFragmentManager, "CallingSupportFragment")
+        isCallingSupportFragmentVisible = true
+    }
+
+    private fun closeCallingSupportFragment() {
+        val fragment = supportFragmentManager.findFragmentByTag("CallingSupportFragment") as? DialogFragment
+        fragment?.dismiss()
+        isCallingSupportFragmentVisible = false
     }
 }
