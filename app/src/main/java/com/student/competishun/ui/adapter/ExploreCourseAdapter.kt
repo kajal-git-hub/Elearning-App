716fe8c761e08bf764bc9b140cfe23c0313cdba4
aryanmaheshwariantino <160240685+aryanmaheshwariantino@users.noreply.github.com>
2024-08-08 12:58:23 +0530

    added myCourse UI and navigation setup

---
+++ app/src/main/java/com/student/competishun/ui/adapter/ExploreCourseAdapter.kt
@@ -0,0 +1,45 @@
+package com.student.competishun.ui.adapter
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.student.competishun.data.model.ExploreCourse
+import com.student.competishun.databinding.ExploreCourseItemBinding
+
+class ExploreCourseAdapter(
+    private val courses: List<ExploreCourse>,
+    private val onItemClicked: (ExploreCourse) -> Unit
+) : RecyclerView.Adapter<ExploreCourseAdapter.ExploreCourseViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExploreCourseViewHolder {
+        val binding = ExploreCourseItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ExploreCourseViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ExploreCourseViewHolder, position: Int) {
+        val course = courses[position]
+        holder.bind(course)
+        holder.itemView.setOnClickListener {
+            onItemClicked(course)
+        }
+    }
+
+    override fun getItemCount(): Int = courses.size
+
+    class ExploreCourseViewHolder(private val binding: ExploreCourseItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(course: ExploreCourse) {
+            binding.tvExploreCourseName.text = course.name
+            binding.tvTag1ExploreCourse.text = course.className
+            binding.tvTag2ExploreCourse.text = course.courseType
+            binding.tvTag3ExploreCourse.text = course.target
+            binding.tvOngoing.text = course.ongoingStatus
+            binding.tvPercentCompleted.text = buildString {
+                append(course.percentCompleted.toString())
+                append("%")
+            }
+            binding.customProgressIndicator.progress = course.percentCompleted
+        }
+    }
+}
