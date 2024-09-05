0a10de7761f0391c4bded3dc0d94fbdae4a22e08
amanshukla1809 <aman@antino.com>
2024-09-05 12:46:27 +0530

    about us page

---
+++ app/src/main/java/com/student/competishun/ui/fragment/AboutUsFragment.kt
@@ -0,0 +1,20 @@
+package com.student.competishun.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.student.competishun.R
+
+class AboutUsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_about_us, container, false)
+    }
+
+}
