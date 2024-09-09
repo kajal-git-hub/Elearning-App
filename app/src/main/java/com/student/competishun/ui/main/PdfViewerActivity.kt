5c56066f84a8acfcc753c6f53ee595ebdb3b9e6b
aryanmaheshwariantino <160240685+aryanmaheshwariantino@users.noreply.github.com>
2024-09-10 00:44:29 +0530

    added ui component in subject content

--- app/src/main/java/com/student/competishun/ui/main/PdfViewerActivity.kt
+++ app/src/main/java/com/student/competishun/ui/main/PdfViewerActivity.kt
@@ -2,6 +2,7 @@ package com.student.competishun.ui.main
 
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import android.webkit.WebView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -19,7 +20,9 @@ class PdfViewerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_pdf_viewer)
-
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE
+        )
         webView = findViewById(R.id.webView)
 
         val pdfUrl = intent.getStringExtra("PDF_URL")
