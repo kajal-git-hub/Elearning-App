b95ed919e967b6172cbba25ccb8ef5dfdf31d3d9
Kajal <kajal@antino.com>
2024-09-10 18:23:31 +0530

    recommended gone visible

--- app/src/main/java/com/student/competishun/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/student/competishun/ui/fragment/HomeFragment.kt
@@ -227,7 +227,7 @@ class HomeFragment : Fragment() {
 
         _binding?.progressBar?.visibility = View.VISIBLE
         _binding?.rvOurCourses?.visibility = View.GONE
-
+        binding.clRecommendedCourses.visibility = View.GONE
         coursesCategoryViewModel.coursesCategory.observe(viewLifecycleOwner, Observer { category ->
             _binding?.progressBar?.visibility = View.GONE
 
@@ -253,6 +253,7 @@ class HomeFragment : Fragment() {
         })
         coursesCategoryViewModel.fetchCoursesCategory()
         getMyDetails()
+        getAllCoursesForStudent("")
 
 //        val filters = FindAllCourseInput(
 //            exam_type = Optional.Absent,
@@ -467,9 +468,10 @@ class HomeFragment : Fragment() {
         studentCoursesViewModel.fetchCourses(filters)
 
         binding.progressBarRec.visibility = View.VISIBLE
-        binding.clRecommendedCourses.visibility = View.GONE
+
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             studentCoursesViewModel.courses.collect { result ->
+                Log.e("TotalStudentCours", result.toString())
                 result?.onSuccess { data ->
                     Log.e("TotalStudentCourses", data.toString())
                     val courses = data.getAllCourseForStudent.courses.map { course ->
