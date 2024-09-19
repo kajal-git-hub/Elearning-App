caae3952b62ff765533c4b3f34dea56b9c49bc34
amanshukla1809 <aman@antino.com>
2024-09-19 12:43:55 +0530

    bookmark ui completed

--- app/src/main/java/com/student/competishun/ui/fragment/BottomsheetBookmarkMoreOptionsFragment.kt
+++ app/src/main/java/com/student/competishun/ui/fragment/BottomsheetBookmarkMoreOptionsFragment.kt
@@ -5,21 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.student.competishun.R
-import com.student.competishun.databinding.FragmentBookMarkBinding
+import com.student.competishun.databinding.FragmentBottomsheetBookmarkMoreOptionsBinding
 
-class BookMarkFragment : Fragment() {
+class BottomSheetBookmarkMoreOptionsFragment : BottomSheetDialogFragment() {
 
     private val binding by lazy {
-        FragmentBookMarkBinding.inflate(layoutInflater)
+        FragmentBottomsheetBookmarkMoreOptionsBinding.inflate(layoutInflater)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        // Inflate the layout for this fragment
         return binding.root
     }
-
 }
--- app/src/main/java/com/student/competishun/ui/fragment/BottomsheetDownloadCompletedFragment.kt
+++ app/src/main/java/com/student/competishun/ui/fragment/BottomsheetDownloadCompletedFragment.kt
@@ -5,21 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.student.competishun.R
-import com.student.competishun.databinding.FragmentBookMarkBinding
+import com.student.competishun.databinding.FragmentBottomsheetDownloadCompletedBinding
 
-class BookMarkFragment : Fragment() {
+class BottomsheetDownloadCompletedFragment : BottomSheetDialogFragment() {
 
     private val binding by lazy {
-        FragmentBookMarkBinding.inflate(layoutInflater)
+        FragmentBottomsheetDownloadCompletedBinding.inflate(layoutInflater)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        // Inflate the layout for this fragment
         return binding.root
     }
-
 }
