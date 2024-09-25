f6dfa36d9c75956e23c2fb1525b446530b9da880
aryanmaheshwariantino <160240685+aryanmaheshwariantino@users.noreply.github.com>
2024-09-25 18:35:33 +0530

    fixed otp issues

--- app/src/main/java/com/student/competishun/ui/fragment/VerifyOTPFragment.kt
+++ app/src/main/java/com/student/competishun/ui/fragment/VerifyOTPFragment.kt
@@ -130,7 +130,7 @@ class VerifyOTPFragment : Fragment() {
                     userDetailsResult.onSuccess { data ->
                         val userDetails = data.getMyDetails
 
-                        if (userDetails.userInformation.address?.city!=null && userDetails.userInformation.reference!=null && userDetails.userInformation.targetYear!=null && userDetails.userInformation.preparingFor!=null && userDetails.fullName!=null) {
+                        if (userDetails.userInformation.address?.city!=null && userDetails.userInformation.reference!=null && userDetails.userInformation.targetYear!=null && userDetails.userInformation.preparingFor!=null && userDetails.fullName!=null ) {
 
                             navigateToHomeActivity(userDetails.id)
                         } else {
