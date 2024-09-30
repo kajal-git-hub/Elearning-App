ae4a589756d9a0a808aa247be50d80ff15bf1a44
aryanmaheshwariantino <160240685+aryanmaheshwariantino@users.noreply.github.com>
2024-10-01 00:59:15 +0530

    create adapter for mypurchase

---
+++ app/src/main/java/com/student/competishun/ui/adapter/PurchaseStatusAdapter.kt
@@ -0,0 +1,34 @@
+package com.student.competishun.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textview.MaterialTextView
+import com.student.competishun.R
+
+class PurchaseStatusAdapter(
+    private val context: Context,
+    private val statusList: List<PurchaseStatus>
+) : RecyclerView.Adapter<PurchaseStatusAdapter.StatusViewHolder>() {
+
+    class StatusViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val statusTextView: MaterialTextView = itemView.findViewById(R.id.et_orderStatus)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StatusViewHolder {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.item_filter_purchase, parent, false)
+        return StatusViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StatusViewHolder, position: Int) {
+        val status = statusList[position]
+        holder.statusTextView.text = status.statusText
+    }
+
+    override fun getItemCount(): Int = statusList.size
+}
+data class PurchaseStatus(val statusText: String)
+
--- app/src/main/java/com/student/competishun/ui/fragment/MyPurchaseFragment.kt
+++ app/src/main/java/com/student/competishun/ui/fragment/MyPurchaseFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.student.competishun.R
+import com.student.competishun.data.model.CoursePaymentDetails
 import com.student.competishun.databinding.FragmentMyPurchaseBinding
+import com.student.competishun.ui.adapter.CoursePaymentAdapter
+import com.student.competishun.ui.adapter.PurchaseStatus
+import com.student.competishun.ui.adapter.PurchaseStatusAdapter
 import com.student.competishun.ui.main.HomeActivity
 
 class MyPurchaseFragment : Fragment() {
@@ -29,9 +35,70 @@ class MyPurchaseFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         (activity as? HomeActivity)?.showBottomNavigationView(false)
         (activity as? HomeActivity)?.showFloatingButton(false)
+
+        val statusList = listOf(
+            PurchaseStatus("All"),
+            PurchaseStatus("Successful"),
+            PurchaseStatus("Failed"),
+            PurchaseStatus("Processing")
+        )
+
+        binding.rvToggleButtons.apply {
+            layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
+            adapter = PurchaseStatusAdapter(context, statusList)
+        }
+
+        val coursePaymentList = listOf(
+            CoursePaymentDetails(
+                "COMPLETE",
+                "Prakhar Integrated (Fast Lane-2) 2024-25",
+                R.drawable.group_1707479053,
+                "₹ 32,500",
+                "26 July, 2024",
+                "One-Time Payment",
+                "24111001",
+                false // Refund note visible
+            ),
+            CoursePaymentDetails(
+                "FAILED",
+                "Prakhar Integrated (Fast Lane-2) 2024-25",
+                R.drawable.failed_status,
+                "₹ 32,500",
+                "26 July, 2024",
+                "One-Time Payment",
+                "24111001",
+                false // Refund note visible
+            ),
+            CoursePaymentDetails(
+                "REFUND COMPLETE",
+                "Prakhar Integrated (Fast Lane-2) 2024-25",
+                R.drawable.refund_status,
+                "₹ 32,500",
+                "26 July, 2024",
+                "One-Time Payment",
+                "24111001",
+                true // Refund note visible
+            ),
+            CoursePaymentDetails(
+                "PAYMENT PROCESSING",
+                "Prakhar Integrated (Fast Lane-2) 2024-25",
+                R.drawable.process_status,
+                "₹ 32,500",
+                "26 July, 2024",
+                "One-Time Payment",
+                "24111001",
+                true // Refund note visible
+            ),
+        )
+
+
+        binding.recyclerViewPurchases.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = CoursePaymentAdapter(coursePaymentList)
+        }
+
     }
 
 }
