7eb211c449b49a4e6803cc68c220fc2e84aef131
amanshukla1809 <aman@antino.com>
2024-10-03 15:53:25 +0530

    yt courses ui completed

---
+++ app/src/main/java/com/student/competishun/ui/fragment/BottomsheetSelectVideoQualityFragment.kt
@@ -0,0 +1,27 @@
+package com.student.competishun.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.student.competishun.R
+
+
+class BottomsheetSelectVideoQualityFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(
+            R.layout.fragment_bottomsheet_select_video_quality,
+            container,
+            false
+        )
+    }
+
+
+}
