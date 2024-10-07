0bd25690263bed545c96e9ffe886c0d503ff91c6
Kajal <kajal@antino.com>
2024-10-07 12:27:51 +0530

    version changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "xyz.penpencil.competishun"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 80
+        versionName = "8.0"
         val razorpayKeyId = findProperty("RAZORPAY_KEY_ID") as String? ?: ""
         buildConfigField("String", "RAZORPAY_KEY_ID", "\"$razorpayKeyId\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
