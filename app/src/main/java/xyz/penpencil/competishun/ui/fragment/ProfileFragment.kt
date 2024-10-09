57180b6dfd664fdf631f9bd430932d41333cedb6
aryanmaheshwariantino <160240685+aryanmaheshwariantino@users.noreply.github.com>
2024-10-09 17:06:49 +0530

    Profile Call Added

--- app/src/main/java/xyz/penpencil/competishun/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/xyz/penpencil/competishun/ui/fragment/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package xyz.penpencil.competishun.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -50,7 +52,13 @@ class ProfileFragment : Fragment() {
 //        Log.d("studentClass",studentClass)
 
         binding.igProfileCall.setOnClickListener {
-
+            val phoneNumber = "8888000021"
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:$phoneNumber")
+            startActivity(intent)
+        }
+        binding.etProfileHelp.setOnClickListener {
+            findNavController().navigate(R.id.ContactUs)
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
