8e417a811dd399fbe0303c6a5c213dc9e9d9a248
pankaj <pankaj.ku@antino.com>
2024-10-17 22:52:17 +0530

    fix after course buy auto reopen issue

--- app/src/main/java/xyz/penpencil/competishun/ui/fragment/PersonalDetailFragment.kt
+++ app/src/main/java/xyz/penpencil/competishun/ui/fragment/PersonalDetailFragment.kt
@@ -108,7 +108,10 @@ class PersonalDetailsFragment : Fragment(), BottomSheetTSizeFragment.OnTSizeSele
             if (isFormValid()) {
                 updateUserDetails()
                 it.findNavController().let { nav->
-                   nav.navigate(getFragmentId())
+                   nav.navigate(getFragmentId(), Bundle().apply {
+                       putStringArray("IDS_FIELDS_LIST", fieldsToVisible.toTypedArray())
+                       putString("IDS", courseId)
+                   })
                    /* val navOptions = NavOptions.Builder()
                         .setPopUpTo(nav.graph.startDestinationId, true)
                         .build()
@@ -149,10 +152,12 @@ class PersonalDetailsFragment : Fragment(), BottomSheetTSizeFragment.OnTSizeSele
             }
         })
     }
+    //987eab08-607c-4e0e-91be-3d44a5ab5ef5
 
     private fun getFragmentId(): Int {
         if (fieldsToVisible.isEmpty()){
-            sharedPreferencesManager.putBoolean(courseId, value = true)
+            sharedPreferencesManager.putBoolean(courseId, true)
+            sharedPreferencesManager.putString("current$courseId", "")
             R.id.courseEmptyFragment
         }
         return if (fieldsToVisible.contains("PASSPORT_SIZE_PHOTO") || fieldsToVisible.contains("AADHAR_CARD")) {
@@ -162,7 +167,8 @@ class PersonalDetailsFragment : Fragment(), BottomSheetTSizeFragment.OnTSizeSele
             sharedPreferencesManager.putString("current$courseId", "address")
             R.id.AddressDetailFragment
         }else {
-            sharedPreferencesManager.removeKey("current$courseId")
+            sharedPreferencesManager.putString("current$courseId", "")
+            sharedPreferencesManager.putBoolean(courseId, true)
             R.id.courseEmptyFragment
         }
     }
@@ -222,7 +228,10 @@ class PersonalDetailsFragment : Fragment(), BottomSheetTSizeFragment.OnTSizeSele
             }
         }
         findNavController().let { nav->
-            nav.navigate(id)
+            nav.navigate(id, Bundle().apply {
+                putStringArray("IDS_FIELDS_LIST", fieldsToVisible.toTypedArray())
+                putString("IDS", courseId)
+            })
            /* val navOptions = NavOptions.Builder()
                 .setPopUpTo(nav.graph.startDestinationId, true)
                 .build()
