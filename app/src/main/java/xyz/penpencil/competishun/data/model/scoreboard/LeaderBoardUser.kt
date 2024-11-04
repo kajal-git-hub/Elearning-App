5d0e441f31f650b5e56d01d413d1064d560487c7
aryanmaheshwariantino <160240685+aryanmaheshwariantino@users.noreply.github.com>
2024-11-04 15:20:51 +0530

    created leaderboard ui

---
+++ app/src/main/java/xyz/penpencil/competishun/data/model/scoreboard/LeaderBoardUser.kt
@@ -0,0 +1,7 @@
+package xyz.penpencil.competishun.data.model.scoreboard
+
+data class LeaderBoardUser(
+    val number: String,
+    val name: String,
+    val score: String
+)
--- app/src/main/java/xyz/penpencil/competishun/ui/fragment/scoreboard/ScoreboardLeaderboardFragment.kt
+++ app/src/main/java/xyz/penpencil/competishun/ui/fragment/scoreboard/ScoreboardLeaderboardFragment.kt
@@ -6,8 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import xyz.penpencil.competishun.R
+import xyz.penpencil.competishun.data.model.scoreboard.LeaderBoardUser
+import xyz.penpencil.competishun.databinding.FragmentScoreboardLeaderboardBinding
+import xyz.penpencil.competishun.ui.adapter.scoreboard.LeaderBoardAdapter
 
 class ScoreboardLeaderboardFragment : Fragment() {
+
+    private lateinit var binding : FragmentScoreboardLeaderboardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +24,21 @@ class ScoreboardLeaderboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scoreboard_leaderboard, container, false)
+        binding = FragmentScoreboardLeaderboardBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val userList = listOf(
+            LeaderBoardUser("4","Abhishek Jain","360"),
+            LeaderBoardUser("5","Surbhi Srivastava ","240"),
+            LeaderBoardUser("6","Amar Soni ","150"),
+            LeaderBoardUser("7","Riya Srivastava ","180"),
+        )
+        val adapter = LeaderBoardAdapter(userList)
+        binding.rvNameScore.adapter = adapter
 
     }
 
