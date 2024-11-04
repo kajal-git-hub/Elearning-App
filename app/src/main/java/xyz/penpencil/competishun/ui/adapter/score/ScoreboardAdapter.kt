9a361c4457493354acd9590c85abee2fa1bd5cc4
pankaj <pankaj.ku@antino.com>
2024-11-04 12:30:48 +0530

    UI changes

---
+++ app/src/main/java/xyz/penpencil/competishun/ui/adapter/score/ScoreboardAdapter.kt
@@ -0,0 +1,43 @@
+package xyz.penpencil.competishun.ui.adapter.score
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.student.competishun.data.model.TestItem
+import android.os.Bundle
+import androidx.navigation.findNavController
+import xyz.penpencil.competishun.R
+
+
+class ScoreboardAdapter(private var testTypeList: List<TestItem>):
+    RecyclerView.Adapter<ScoreboardAdapter.AcademicViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AcademicViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_test_academic, parent, false)
+        return AcademicViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = testTypeList.size
+
+    override fun onBindViewHolder(holder: AcademicViewHolder, position: Int) {
+        val item = testTypeList[position]
+//        holder.bind(item)
+        holder.itemView.setOnClickListener {
+            val bundle = Bundle().apply {
+                putInt("testId", 0)
+                putString("testName", "")
+            }
+            it.findNavController().navigate(R.id.action_academicTestFragment_to_testDetailFragment, bundle)
+        }
+    }
+
+    inner class AcademicViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        /* private val titleText: TextView = itemView.findViewById(R.id.title)
+         private val type: ImageView = itemView.findViewById(R.id.type)
+
+         fun bind(item: TestItem) {
+             titleText.text = item.title
+             type.visibility = if (item.isFilter) View.VISIBLE else View.GONE
+         }*/
+    }
+}
--- app/src/main/java/xyz/penpencil/competishun/ui/fragment/scoreboard/ScoreDashboardFragment.kt
+++ app/src/main/java/xyz/penpencil/competishun/ui/fragment/scoreboard/ScoreDashboardFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.MaterialToolbar
+import com.student.competishun.data.model.TestItem
 import xyz.penpencil.competishun.R
 import xyz.penpencil.competishun.databinding.FragmentScoreDashboardBinding
 import xyz.penpencil.competishun.databinding.FragmentTestDashboardBinding
+import xyz.penpencil.competishun.ui.adapter.score.ScoreboardAdapter
+import xyz.penpencil.competishun.ui.adapter.test.AcademicTestAdapter
+import xyz.penpencil.competishun.ui.adapter.test.AcademicTestResumeAdapter
+import xyz.penpencil.competishun.ui.main.HomeActivity
 
 
 class ScoreDashboardFragment : Fragment() {
@@ -15,12 +25,61 @@ class ScoreDashboardFragment : Fragment() {
     private var _binding: FragmentScoreDashboardBinding? = null
     private val binding get() = _binding!!
 
+    private var scoreboardAdapter: ScoreboardAdapter?=null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentScoreDashboardBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+
+        initAdapter()
+    }
+
+    private fun initUI(){
+        (activity as? HomeActivity)?.setSupportActionBar(binding.toolbar)
+        binding.toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_search -> {
+
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.toolbar.setNavigationIcon(R.drawable.arrow_left_white)
+        binding.toolbar.setNavigationOnClickListener {
+            it.findNavController().popBackStack()
+        }
+
+    }
+
+
+    private fun initAdapter(){
+        scoreboardAdapter = ScoreboardAdapter(listOf(
+            TestItem("Filter", true),
+            TestItem("JEE-Mains", false),
+            TestItem("JEE-Advanced", false),
+            TestItem("NEET-UG", false),
+            TestItem("NEET-UG", false),
+            TestItem("NEET-UG", false),
+            TestItem("NEET-UG", false),
+            TestItem("NEET-UG", false),
+            TestItem("NEET-UG", false),
+            TestItem("NEET-UG", false),
+            TestItem("NEET-UG", false),
+            TestItem("AIIMS", false)
+        ))
+        binding.rvScore.layoutManager = LinearLayoutManager(context,
+            LinearLayoutManager.VERTICAL, false)
+        binding.rvScore.adapter = scoreboardAdapter
     }
 
     override fun onDestroyView() {
