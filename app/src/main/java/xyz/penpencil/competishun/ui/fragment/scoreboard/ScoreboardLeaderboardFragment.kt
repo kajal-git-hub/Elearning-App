a3bf54a37747d0021210f9f50b65a8f2a59314bb
aryanmaheshwariantino <160240685+aryanmaheshwariantino@users.noreply.github.com>
2024-11-04 12:51:19 +0530

    commit for switch

---
+++ app/src/main/java/xyz/penpencil/competishun/ui/fragment/scoreboard/ScoreboardLeaderboardFragment.kt
@@ -0,0 +1,31 @@
+package xyz.penpencil.competishun.ui.fragment.scoreboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import xyz.penpencil.competishun.R
+
+class ScoreboardLeaderboardFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scoreboard_leaderboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
