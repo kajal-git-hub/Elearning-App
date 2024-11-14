9a4da452ac64a4762aa60bdb8192d17ca3a0faf7
aryanmaheshwariantino <160240685+aryanmaheshwariantino@users.noreply.github.com>
2024-11-14 18:22:51 +0530

    fixed youtube player

--- app/src/main/java/xyz/penpencil/competishun/ui/fragment/scoreboard/BarChartFragment.kt
+++ app/src/main/java/xyz/penpencil/competishun/ui/fragment/scoreboard/BarChartFragment.kt
@@ -1,15 +1,26 @@
 package xyz.penpencil.competishun.ui.fragment.scoreboard
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+import xyz.penpencil.competishun.R
 import xyz.penpencil.competishun.databinding.FragmentBarChartBinding
 
 class BarChartFragment : Fragment() {
 
     private lateinit var binding : FragmentBarChartBinding
+
+    private lateinit var barChart:BarChart
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +42,26 @@ class BarChartFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-//        barChart.findViewById(R.id.bar_chart)
+        barChart=  view.findViewById(R.id.bar_chart)
+
+        val list : ArrayList<BarEntry> = ArrayList()
+
+        list.add(BarEntry(100f,100f))
+        list.add(BarEntry(101f,102f))
+        list.add(BarEntry(103f,107f))
+        list.add(BarEntry(104f,109f))
+
+        val barDataSet = BarDataSet(list,"List")
+
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS,255)
+        barDataSet.valueTextColor = Color.BLACK
+        val barData = BarData(barDataSet)
+        barChart.setFitBars(true)
+        barChart.data = barData
+
+        barChart.description.text = "Bar Chart"
+
+        barChart.animateY(2000)
 
 
 
